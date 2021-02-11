07f9ee08d18313fb9672e4c90641c3b71f0de397
Jennifer Field <miss.j.field@Gmail.com>
2021-02-11 11:08:56 +0000

    Adding scene manager back

---
+++ Going_Quackers_Framework/Code/EngineBase/SceneManager/Scene.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+#include <string>
+
+class GameObject;
+
+class Scene
+{
+public:
+	Scene(std::string as_ID, std::string as_DisplayName, std::string as_Type);
+	~Scene();
+
+	void OnSceneEnter();
+	void OnSceneExit();
+
+	void AddObject(GameObject* ap_newObject);
+
+	void Update(float af_deltaTime);
+	void Draw();
+
+	std::string GetSceneDisplayName() { return ms_SceneDisplayName; };
+	std::string GetSceneID() { return ms_SceneID; };
+
+	std::vector<GameObject*> GetSceneObjects() { return mp_SceneObjects; };
+
+private:
+	std::string ms_SceneDisplayName;
+	std::string ms_SceneID;
+	std::string ms_SceneType;
+
+	std::vector<GameObject*> mp_SceneObjects;
+};
+
