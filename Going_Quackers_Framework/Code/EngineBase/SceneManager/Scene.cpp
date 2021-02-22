c0c0471cb9189348aa0b1bb918ebf4031c9cfe2f
Jennifer Field <miss.j.field@Gmail.com>
2021-02-22 14:30:45 +0000

    Revert "Object Loading added"
    This reverts commit aa8e53ba5f9f4d3cd7199f9fc96aeef4364a4dd7.

--- Going_Quackers_Framework/Code/EngineBase/SceneManager/Scene.cpp
+++ Going_Quackers_Framework/Code/EngineBase/SceneManager/Scene.cpp
@@ -1,5 +1,11 @@
 #include "Scene.h"
 
+/// <summary>
+/// Scene Constructor
+/// </summary>
+/// <param name="as_ID">String containing ID of scene</param>
+/// <param name="as_DisplayName">String containing display name of scene</param>
+/// <param name="as_Type">String containing Type of Scene</param>
 Scene::Scene(std::string as_ID, std::string as_DisplayName, std::string as_Type)
 {
 	ms_SceneID = as_ID;
@@ -7,6 +13,9 @@ Scene::Scene(std::string as_ID, std::string as_DisplayName, std::string as_Type)
 	ms_SceneType = as_Type;
 }
 
+/// <summary>
+/// Scene Deconstructor
+/// </summary>
 Scene::~Scene()
 {
 	for (auto lp_object : mp_SceneObjects) {
@@ -15,19 +24,33 @@ Scene::~Scene()
 	mp_SceneObjects.clear();
 }
 
+/// <summary>
+/// Event to trigger on entering a scene
+/// </summary>
 void Scene::OnSceneEnter()
 {
 }
 
+/// <summary>
+/// Event to trigger on leaving a scene
+/// </summary>
 void Scene::OnSceneExit()
 {
 }
 
+/// <summary>
+/// Add an object to the Scene
+/// </summary>
+/// <param name="ap_newObject">GameObject to add</param>
 void Scene::AddObject(GameObject* ap_newObject)
 {
 	mp_SceneObjects.push_back(ap_newObject);
 }
 
+/// <summary>
+/// Updates the scene
+/// </summary>
+/// <param name="af_deltaTime">delta time</param>
 void Scene::Update(float af_deltaTime)
 {
 	for (auto lp_object : mp_SceneObjects) {
@@ -35,6 +58,9 @@ void Scene::Update(float af_deltaTime)
 	}
 }
 
+/// <summary>
+/// Renders the scene
+/// </summary>
 void Scene::Draw()
 {
 	for (auto lp_object : mp_SceneObjects) {
