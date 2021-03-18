0c6a4aaf55b60be71b273ce9ebd528fd9cbf532e
Jennifer Field <miss.j.field@Gmail.com>
2021-03-18 09:37:15 +0000

    Revert "Merge branch 'SceneManager' into main"
    This reverts commit e77abaf33d77c1a085c161250d0fe1a2f4af5499.

--- Going_Quackers_Framework/Code/EngineBase/SceneManager/SceneManager.h
+++ Going_Quackers_Framework/Code/EngineBase/SceneManager/SceneManager.h
@@ -1,39 +1,11 @@
 #pragma once
 
-#ifndef SCENEMANAGER
-#define SCENEMANAGER
-
 #include "../SceneManager/Scene.h"
-#include "../Data Structures/Vectors.h"
-
-// Object IDs
-enum ObjectIDs {
-	invalidOption,
-	debugSquare
-};
-
-struct objectConfig {
-	std::string id;
-	Vector2 pos;
-	float rotation;
-	float scale;
-	// Vector3 colour
-	std::string texturePath;
-};
 
 class SceneManager
 {
-/* Singleton Stuff */
 public:
-	static SceneManager* GetInstance();
-	SceneManager(SceneManager const&) = delete;
-	void operator = (SceneManager const&) = delete;
-protected:
 	SceneManager();
-	static SceneManager* mp_instance;
-/* Singleton Stuff End */
-
-public:
 	~SceneManager();
 
 	void ChangeScene(std::string as_SceneID);
@@ -44,12 +16,10 @@ public:
 	Scene* GetCurrentScene() { return mp_CurrentScene; };
 
 private:
-	Scene* LoadScene(std::string as_ID);
+	Scene* LoadScene(std::string as_Path);
 	void UnloadScene();
-	ObjectIDs ObjectIDStringToEnum(std::string as_id);
-	//void BuildObjectFromID(objectConfig a_objectConfig);
 
 	Scene* mp_CurrentScene;
+
 };
 
-#endif SCENEMANAGER
