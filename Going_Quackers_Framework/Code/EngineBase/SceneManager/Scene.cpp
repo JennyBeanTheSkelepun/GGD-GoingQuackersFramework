b36831ff0ff34bdace0f5e87b084df79ab5f8942
Eden <eden.bealesharper@gmail.com>
2021-05-14 10:49:46 +0100

    Fixed double collision

--- Going_Quackers_Framework/Code/EngineBase/SceneManager/Scene.cpp
+++ Going_Quackers_Framework/Code/EngineBase/SceneManager/Scene.cpp
@@ -1,4 +1,5 @@
 #include "Scene.h"
+#include "../Game Systems/Components/Physics/Rigidbody.h"
 #include "../Game Systems/GameObject.h"
 
 Scene::Scene(std::string as_ID, std::string as_DisplayName, std::string as_Type)
@@ -33,6 +34,10 @@ void Scene::Update(float af_deltaTime)
 {
 	for (int i = 0; i < mp_SceneObjects.size(); i++) {
 		mp_SceneObjects[i]->Update();
+	}
+
+	for (auto lp_object : mp_SceneObjects) {
+		lp_object->GetComponent<Rigidbody>()->resetCollideFlag();
 	}
 }
 
