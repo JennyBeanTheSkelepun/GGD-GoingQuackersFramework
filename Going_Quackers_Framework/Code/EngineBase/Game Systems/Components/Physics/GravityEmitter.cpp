96eac2de5d92d3e039d13e5cf067f752d7817561
Eden Beales Harper <eden.bealesharper@gmail.com>
2021-05-21 10:11:33 +0100

    ImGui setup for Physics

--- Going_Quackers_Framework/Code/EngineBase/Game Systems/Components/Physics/GravityEmitter.cpp
+++ Going_Quackers_Framework/Code/EngineBase/Game Systems/Components/Physics/GravityEmitter.cpp
@@ -10,16 +10,50 @@ void GravityEmitter::applyGravity(GameObject* callObj, std::vector<GameObject*>*
 		{
 			Vector2 gravityPosition = callObj->GetTransform()->GetPosition();
 
-			switch (callObj->GetComponent<Rigidbody>()->GetCollisionType())
+			switch (m_gravType)
 			{
-			case CollisionTypes::AABB:
+			case GravityTypes::DIRECTION:
 				rb->AddForce(m_gravityDirection * m_gravityStrength);
 				break;
-			case CollisionTypes::Sphere:
+			case GravityTypes::CENTRE:
 				Vector2 force = (gravityPosition - obj->GetTransform()->GetPosition()).Normalize() * m_gravityStrength;
 				obj->GetComponent<Rigidbody>()->AddForce(force);
 				break;
 			}
 		}
+	}
+}
+
+void GravityEmitter::ImGuiSetup()
+{
+	ImGui::InputFloat("Gravity Strength", &m_gravityStrength);
+
+	if (ImGui::BeginCombo("Gravity Type", m_GravTypeDropdownSelected))
+	{
+		for (int i = 0; i < IM_ARRAYSIZE(m_GravTypeDropdown); i++)
+		{
+			bool is_selected = (m_GravTypeDropdownSelected == m_GravTypeDropdown[i]);
+
+			if (ImGui::Selectable(m_GravTypeDropdown[i], is_selected))
+			{
+				m_GravTypeDropdownSelected = m_GravTypeDropdown[i];
+
+				if (m_GravTypeDropdownSelected == "Direction")
+				{
+					m_gravType = GravityTypes::DIRECTION;
+				}
+				else if (m_GravTypeDropdownSelected == "Centre")
+				{
+					m_gravType = GravityTypes::CENTRE;
+				}
+			}
+
+			if (is_selected)
+			{
+				ImGui::SetItemDefaultFocus();
+			}
+		}
+
+		ImGui::EndCombo();
 	}
 }
