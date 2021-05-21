c06b4c916cbfbabda727cbc5f03617967a76b40f
MajicGoale <48091148+MajicGoale@users.noreply.github.com>
2021-05-21 16:37:53 +0100

    added grabwall function to player
    added the grab wall function to player without the functionality yet

--- Going_Quackers_Framework/Code/EngineBase/Game Systems/Components/Player.cpp
+++ Going_Quackers_Framework/Code/EngineBase/Game Systems/Components/Player.cpp
@@ -1,5 +1,19 @@
 #include "Player.h"
 
 Player::Player(GameObject* owner) : Component(owner, ComponentTypes::PLAYER) {
+	grabbed = false;
+	//Component rigidbody = owner->GetComponent(RIGIDBODY);
+}
 
+void Player::GrabWall()
+{
+	//todo make an if statement that checks if the player is both colliding with something and that the grappling hook rope is very small/the hook is a certain distance to the player and sets grabbed to true
+
+
+	if (grabbed)
+	{
+		//todo get the object the player is colliding with
+		//todo set the player to face the wall they are colliding with
+		//todo set the players to be constantly moving towards the wall it's attached to
+	}
 }
--- Going_Quackers_Framework/Code/EngineBase/Game Systems/Components/Player.h
+++ Going_Quackers_Framework/Code/EngineBase/Game Systems/Components/Player.h
@@ -3,11 +3,15 @@
 #pragma once
 
 #include "Component.h"
+#include "C:\Users\m013002i\Documents\GitHub\GGD-GoingQuackersFramework\Going_Quackers_Framework\Code\EngineBase\Game Systems\GameObject.h"
 
 class Player : public Component
 {
 public:
 	Player(GameObject* owner);
+	bool grabbed;
+private:
+	void GrabWall();
 };
 
 #endif // !PLAYER_H
