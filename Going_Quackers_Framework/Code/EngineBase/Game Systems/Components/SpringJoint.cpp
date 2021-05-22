18ff1d7da255e8d3a4d23b28d578f8d6b77ad91c
Luke J <lukeajones1@hotmail.co.uk>
2021-05-22 23:51:15 +0100

    Added untested fixed head force application

--- Going_Quackers_Framework/Code/EngineBase/Game Systems/Components/SpringJoint.cpp
+++ Going_Quackers_Framework/Code/EngineBase/Game Systems/Components/SpringJoint.cpp
@@ -1,9 +1,12 @@
 #include "SpringJoint.h"
 #include "../GameObject.h"
+#include "Physics/Rigidbody.h"
 
 SpringJoint::SpringJoint(GameObject* ap_owner, GameObject* ap_connectedObject) : Component(ap_owner, ComponentTypes::SPRINGJOINT) {
 	mp_connectedObject = ap_connectedObject;
 	GetOwner()->AddChild(mp_connectedObject);
+
+	//TODO - check the child has a valid rigidbody before adding it
 }
 
 SpringJoint::~SpringJoint() {
@@ -16,6 +19,8 @@ void SpringJoint::SetConnectedObject(GameObject* ap_connectedObject) {
 
 	mp_connectedObject = ap_connectedObject;
 	GetOwner()->AddChild(mp_connectedObject);
+
+	//TODO - check the child has a valid rigidbody before adding it
 }
 
 void SpringJoint::SetStrength(float af_strength) {
@@ -32,4 +37,29 @@ void SpringJoint::SetSpringType(SpringType a_type) {
 
 void SpringJoint::SetSpringMode(SpringMode a_mode) {
 	m_mode = a_mode;
+}
+
+void SpringJoint::Update() {
+	float currentLength = GetOwner()->GetTransform()->GetPosition().Distance(mp_connectedObject->GetTransform()->GetPosition());
+
+	if (currentLength < mf_desiredLength && m_mode != SpringMode::REPEL_ONLY ||
+		currentLength > mf_desiredLength && m_mode != SpringMode::ATTRACT_ONLY) {
+		if (m_type == SpringType::FIXED_HEAD) {
+			ApplyFixedHeadSpringForce(currentLength);
+		}
+		else
+			ApplyNonFixedHeadSpringForce(currentLength);
+	}
+}
+
+void SpringJoint::ApplyFixedHeadSpringForce(float af_currentStretch) {
+	Vector2 position = GetOwner()->GetTransform()->GetPosition();
+	Vector2 direction = (position - mp_connectedObject->GetTransform()->GetPosition()).Normalize(); //TODO - check this is the right way around
+
+	float force = mf_strength * af_currentStretch;
+
+	mp_connectedObject->GetComponent<Rigidbody>()->AddForce(direction * force);
+}
+
+void SpringJoint::ApplyNonFixedHeadSpringForce(float af_currentStretch) {
 }
