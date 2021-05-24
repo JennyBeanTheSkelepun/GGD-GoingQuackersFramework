52a1227c782b54ed21577dc82edc596c0641869b
davidtaylor6130 <davidtaylor613@gmail.com>
2021-05-24 12:26:37 +0100

    Updated Inputs from boiler plate code

--- Going_Quackers_Framework/Code/EngineBase/Game Systems/Components/SpriteRenderer.h
+++ Going_Quackers_Framework/Code/EngineBase/Game Systems/Components/SpriteRenderer.h
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-#ifndef _SPRITERENDERER_H_
-#define _SPRITERENDERER_H_
-
-#include "Component.h"
-#include <string>
-
-class Graphics;
-
-enum ModelType
-{
-	TWO_DIMENSIONS = 0,
-	THREE_DIMENSIONS = 1
-};
-
-class SpriteRenderer : public Component
-{
-public:
-	SpriteRenderer(GameObject* owner);
-	~SpriteRenderer();
-
-	void Initialze(std::string TextureLocation, std::wstring ShaderLocation);
-
-	inline int GetTexture() { return mi_Texture; };
-	inline int GetShader() { return mi_Shader; };
-
-	void RemoveTextureShader();
-
-	void ImGUIUpdate() override;
-	void OnDestroy() override;
-	void SceneLoad(json* componentJSON) override;
-	json* SceneSave() override;
-
-private:
-
-	int ModelType = ModelType::TWO_DIMENSIONS;
-	int mi_Texture, mi_Shader, mi_ID;
-
-	std::string m_TextureLocation, m_ShaderLocation;
-
-	char* TextureSelectionInput;
-	char* ShaderSelectionInput;
-};
-
-=======
 #ifndef _SPRITERENDERER_H_
 #define _SPRITERENDERER_H_
 
@@ -91,6 +46,4 @@ private:
 	char* TextureSelectionInput;
 	char* ShaderSelectionInput;
 };
-
->>>>>>> Boilerplate-Code
-#endif
+#endif //- _SPRITERENDERER_H_ -//
