137fd294829b7f09dbfb8c840cd6a4794b8507e4
j <engiecore@gmail.com>
2021-05-28 16:55:39 +0100

    Parent/Child Loading rework

--- Going_Quackers_Framework/Code/EngineBase/SceneManager/SceneManager.h
+++ Going_Quackers_Framework/Code/EngineBase/SceneManager/SceneManager.h
@@ -48,14 +48,15 @@ public:
 	inline void SetAutoSave(bool value) { mb_doAutoSave = value; }
 
 private:
-	Scene* LoadScene(std::string as_Path);
-	void UnloadScene(bool as_SaveToJSON);
-
 	Scene* mp_CurrentScene;
-
 	bool mb_doAutoSave;
 
+	Scene* LoadScene(std::string as_Path);
+	void LoadChildren(GameObject* ap_object, json* ap_json);
+	void UnloadScene(bool as_SaveToJSON);
+
 	void SaveToJSON(Scene* ap_Scene);
+	void SaveChildren(GameObject* lp_object, json* ap_json);
 	std::wstring stringToWString(std::string as_string);
 
 	template<typename T>
