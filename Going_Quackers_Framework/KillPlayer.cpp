822f5853d830ece9b84c92c2f5c561454b3c4073
Conor <mccannconor2000@gmail.com>
2021-06-02 14:52:48 +0100

    added kill player cpp and h files

---
+++ Going_Quackers_Framework/KillPlayer.cpp
@@ -0,0 +1,41 @@
+#include "KillPlayer.h"
+
+KillPlayer::KillPlayer(GameObject* owner) : Component(owner, ComponentTypes::KillPLAYER, "Kill Player")
+{
+	KillObject = this->GetOwner();
+}
+KillPlayer::~KillPlayer()
+{
+	KillObject = nullptr;
+}
+void KillPlayer::OnDestroy()
+{
+	this->~KillPlayer();
+}
+void KillPlayer::Update()
+{
+	
+}
+void KillPlayer::ImGUIUpdate()
+{
+	
+}
+json* KillPlayer::SceneSave()
+{
+
+}
+void KillPlayer::SceneLoad(json* componentJSON)
+{
+	
+}
+void KillPlayer::KillThePlayer()
+{
+	if (KillObject->GetComponent<Rigidbody>() == nullptr)
+	{
+		Debug::getInstance()->LogError("the player doesn't have a rigidbody component");
+	}
+	else if (KillObject->AddComponent<Rigidbody>()->GetCollidedObjects().empty() != true)
+	{
+		playerObj = KillObject->AddComponent<Rigidbody>()->GetCollidedObjects();
+	}
+}
---
+++ Going_Quackers_Framework/KillPlayer.h
@@ -0,0 +1,27 @@
+#ifndef KILLPLAYER_H
+#define KILLPLAYER_H
+#pragma once
+
+#include "Code/EngineBase/Game Systems/Components/Component.h"
+#include "Code/EngineBase/Game Systems/Components/Physics/Trigger.h"
+#include "Code/EngineBase/Game Systems/GameObject.h"
+
+class KillPlayer : public Component
+{
+public:
+	KillPlayer(GameObject* owner);
+	~KillPlayer();
+
+	void Update() override;
+	void OnDestroy() override;
+	void ImGUIUpdate() override;
+	json* SceneSave() override;
+	void SceneLoad(json* componentJSON) override;
+private:
+	void KillThePlayer();
+	GameObject* KillObject;
+	std::vector<GameObject*> playerObj;
+
+
+#endif // !KILLPLAYER
+
